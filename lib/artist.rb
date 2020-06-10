e68771c43f7e0dbcc5c8b529c29fbd4fe4cdda4f
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-10 21:15:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,8 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  include Paramable:: InstanceMethods 
+  include Paramable::InstanceMethods 
+  
   attr_accessor :name
   attr_reader :songs
 
